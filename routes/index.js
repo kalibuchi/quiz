192a721e0f1564411aa84afe992996334b08ca43
Iván Sánchez Ortega <ivan@sanchezortega.es>
2015-07-19 23:38:48 +0200

    Añadido /author y quitado /users

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET credits. */
+router.get('/author', function(req, res, next) {
+  res.render('author');
+});
+
 module.exports = router;
